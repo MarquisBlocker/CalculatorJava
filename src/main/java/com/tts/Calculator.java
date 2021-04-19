cddf91467db351bd4813ab2229d9d89c104636e4
MarquisBlocker <mblocker108@rams.wssu.edu>
2021-04-19 16:38:24 -0400

    Initial commit

---
+++ src/main/java/com/tts/Calculator.java
@@ -0,0 +1,35 @@
+package com.tts;
+
+public class Calculator {
+    public static int add(int num1, int num2){
+        int sum;
+        sum = num1 + num2;
+        return sum;
+    }
+
+    public static int subtract(int num1, int num2){
+        int difference;
+        difference = num1 - num2;
+        return difference;
+    }
+
+    public static int multiply(int num1, int num2){
+        int product;
+        product = num1 * num2;
+        return product;
+    }
+
+    public static int divide(int num1, int num2){
+        int quotient;
+        quotient = num1/num2;
+        return quotient;
+    }
+
+    public static int square(int num1, int num2){
+        int squared;
+        squared = num1 * num1;
+        return squared;
+    }
+
+
+}
